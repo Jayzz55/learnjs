5034a7a024f60022f8051e87b46197c92d2a221e
Jaya Wijono <jayzzwijono@yahoo.com>
2016-05-19 17:13:53 +1000

    add 1-way data binding between the view and model

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -18,7 +18,8 @@ describe('LearnJS', function() {
   describe('problem view', function() {
     it('has a title that includes the problem number', function() {
       var view = learnjs.problemView('1');
-      expect(view.text().trim()).toEqual('Problem #1');
+      var result = view.find('.title').text().trim();
+      expect(result).toEqual('Problem #1');
     });
   });
 
